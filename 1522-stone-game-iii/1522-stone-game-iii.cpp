00af38eeeb376ab6ab80c55f97c1e6b66f764db0
vaibav03 <107197417+vaibav03@users.noreply.github.com>
2024-08-21 12:13:13 +0530

    Time: 119 ms (98.93%), Space: 129.1 MB (95.8%) - LeetHub

--- 1522-stone-game-iii/1522-stone-game-iii.cpp
+++ 1522-stone-game-iii/1522-stone-game-iii.cpp
@@ -2,27 +2,28 @@ class Solution {
 public:
     string stoneGameIII(vector<int>& s) {
      int n = s.size();
-     vector<int> dp(n+1,0);
+     int i0{},i1{},i2{};
      int i = n-1;
      while(i>=0){
       int ans=INT_MIN;
 
-      ans=max(ans,s[i]-dp[i+1]);
+      ans=max(ans,s[i]-i0);
 
       if(i+1<s.size())
-      ans=max(ans,s[i+1]+s[i]-dp[i+2]);
+      ans=max(ans,s[i+1]+s[i]-i1);
 
       if(i+2<s.size())
-      ans=max(ans,s[i+2]+s[i+1]+s[i]-dp[i+3]);
-
-      dp[i]=ans;
+      ans=max(ans,s[i+2]+s[i+1]+s[i]-i2);
+      
+      i2=i1;
+      i1=i0;
+      i0=ans;
       i--;
      }
-     int sum = accumulate(s.begin(),s.end(),0)/2;
 
-     if(dp[0]>0)
+     if(i0>0)
      return"Alice";
-     else if(dp[0]==0)
+     else if(i0==0)
      return "Tie";
      else
      return "Bob"; 
