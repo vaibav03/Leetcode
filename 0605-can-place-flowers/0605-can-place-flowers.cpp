4e452cd9346624db28326e57f6505ecdb7d1eae0
vaibav03 <107197417+vaibav03@users.noreply.github.com>
2024-11-01 20:26:24 +0530

    Time: 0 ms (100%), Space: 23.1 MB (54.35%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -4,7 +4,6 @@ public:
         int ans = 0;
         
         for (int i = 0; i < flowerbed.size(); i++) {
-            // Check if the current spot is empty and its adjacent spots are also empty
             if (flowerbed[i] == 0 &&
                 (i == 0 || flowerbed[i - 1] == 0) &&
                 (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)) {
