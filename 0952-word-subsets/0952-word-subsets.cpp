098032240401a76b08bdca76da5561208867cccf
vaibav03 <107197417+vaibav03@users.noreply.github.com>
2025-01-10 23:42:18 +0530

    Time: 345 ms (5.94%), Space: 184.8 MB (5.02%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        map<char,int> M;
+        for(int i=0;i<words2.size();i++){
+            map<char,int> ch;
+            for(int j=0;j<words2[i].size();j++){
+                ch[words2[i][j]]++;
+                M[words2[i][j]] = max(M[words2[i][j]],ch[words2[i][j]]);
+            }
+        }
+
+        vector<string> ans;
+        for(int i=0;i<words1.size();i++){
+            map<char,int> temp = M;
+            for(int j=0;j<words1[i].size();j++){
+                temp[words1[i][j]]--;
+            }
+            bool isAns = true;
+            for(auto x : temp){
+                if(x.second>=0 && x.second!=0)
+                isAns = false;
+            }
+            if(isAns) ans.push_back(words1[i]);
+        }
+        return ans;
+    }
+};
