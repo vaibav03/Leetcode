d916e097fdb774dbcf603af99e7151eff45f6efd
vaibav03 <107197417+vaibav03@users.noreply.github.com>
2025-04-19 22:50:00 +0530

    Time: 59 ms (46.51%), Space: 60.5 MB (38.74%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    // Lower bound function to find the first index where nums[index] >= x
-    int lb(vector<int>& nums, int x, int l, int r) {
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] < x)
-                l = mid + 1;
-            else
-                r = mid - 1;
+    long long lower_bound(vector<int>& nums, int low, int high, int element) {
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+            if (nums[mid] >= element) {
+                high = mid - 1;
+            } else
+                low = mid + 1;
         }
-        return l;
+        return low;
     }
-
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
         sort(nums.begin(), nums.end());
         long long ans = 0;
-
         for (int i = 0; i < nums.size(); i++) {
-            // Use `lb` to find the range where nums[j] is within the range
-            int leftBound = lb(nums, lower - nums[i], i + 1, nums.size() - 1);
-            int rightBound = lb(nums, upper - nums[i] + 1, i + 1, nums.size() - 1);
+            int low =
+                lower_bound(nums, i + 1, nums.size() - 1, lower - nums[i]);
 
-            // Count valid pairs where j > i and nums[i] + nums[j] is within [lower, upper]
-            ans += (rightBound - leftBound);
+            int high =
+                lower_bound(nums, i + 1, nums.size() - 1, upper - nums[i] + 1);
+
+            ans += 1LL * (high - low);
         }
-
         return ans;
     }
 };
