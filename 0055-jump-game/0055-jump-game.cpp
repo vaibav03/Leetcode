af8408f8bb1fd49620d92ff13dcc2bdfa204d470
vaibav03 <107197417+vaibav03@users.noreply.github.com>
2025-05-28 07:14:32 +0530

    Time: 758 ms (5.14%), Space: 54.9 MB (9.69%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
+
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n,0);
-        dp[n-1]=true;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]==0) dp[i]=0;
-          bool ans{};  
-          for(int j=1;j<=nums[i];j++){
-          if(i+j<nums.size())
-          ans|=dp[i+j];
-          else 
-          {
-           ans=true;
-           break; 
-          } 
-          }
-          dp[i]=ans;
+        if(nums.size() == 1) return true;
+        vector<int> vis(nums.size(),0);
+        int least = INT_MAX;
+        for(int i = nums.size()-2;i>=0;i--){
+            if(nums[i]+i >= nums.size()-1 || nums[i]+i>=least){
+                 vis[i] = true;
+                 least = min(least,i);
+                 }
+
         }
-        return dp[0];
-
-    }
-
-    bool f(vector<int>nums,int i)
-    {
-      if(i>=nums.size()) return true;
-      else if(nums[i]==0) {return false;}
-
-      bool ans = false;
-      for(int j=1;j<=nums[i];j++)
-       ans|=f(nums,i+j);
-
-     return ans;    
+        for(int i=0;i<nums.size();i++){
+            cout<<vis[i]<<" ";
+        }
+        cout<<endl;
+        return vis[0];
     }
 };
