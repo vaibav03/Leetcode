09aaa2dc14f04636bae44b382b484b8eaa0c3d85
vaibav03 <107197417+vaibav03@users.noreply.github.com>
2025-09-17 13:00:19 -0700

    Time: 0 ms (100%), Space: 10.4 MB (87.41%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    bool solve(string s ,vector<string> & wordDict,int l,int r,vector<vector<int>> &dp){
-        if(dp[l][r] != -1) return dp[l][r];
 
-        for(int diff = 0;diff<=r-l+1;diff++){
-            for(int ind=0;ind<wordDict.size();ind++){
-                if(wordDict[ind] == s.substr(l,diff)){
-                    if(l+diff == s.size()) return dp[l][r] = true;
-
-                   bool ans =  solve(s,wordDict,l+diff,r,dp);
-                   if(ans==true)
-                   return dp[l][r] = ans;
-                }
-            }
-        }
-        return dp[l][r] = false;
-    }
     bool wordBreak(string s, vector<string>& wordDict) {
-        int n = s.size()-1;
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return solve(s,wordDict,0,s.size()-1,dp);
+        int n = s.size();
+        vector<bool> dp(n+1);
+        for(auto word : wordDict){
+            int len = word.size();
+            if( len<=n && s.substr(0,len) == word ) dp[word.size()-1] = true;
+        }
+
+        for(int i=0;i<n;i++){
+            for(auto word : wordDict){
+                int len = word.size();
+                if(len<=i  && dp[i-len] && s.substr(i-len+1,len) == word  ) dp[i] = true;
+            } 
+        }
+        for(int i=0;i<n;i++) cout<<dp[i]<<" ";
+        cout<<endl;
+        return dp[n-1];
     }
 };
