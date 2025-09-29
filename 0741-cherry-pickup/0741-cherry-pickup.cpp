b17e82232ac61d5abc97c21759a01aae09f21835
vaibav03 <107197417+vaibav03@users.noreply.github.com>
2025-09-29 15:37:00 -0700

    Time: 43 ms (63.27%), Space: 28.1 MB (62.88%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.cpp
+++ 0741-cherry-pickup/0741-cherry-pickup.cpp
@@ -4,39 +4,18 @@ public:
 int cherryPickup(vector<vector<int>>& grid) {
     int n = grid.size(), m = grid[0].size();
     vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(m+1, vector<int>(m+1, INT_MIN)));
-
-    // base case
-    if (grid[0][0] == -1) return 0;
     dp[1][1][1] = grid[0][0];
-
-    for (int i=1; i<=n; i++) {
-        for (int j=1; j<=m; j++) {
-            for (int k=1; k<=m; k++) {
-                if (i+j-k < 1 || i+j-k > n || 
-                    grid[i-1][j-1] == -1 || 
-                    grid[i+j-k-1][k-1] == -1) continue;
-
-                if (i==1 && j==1 && k==1) continue; // already initialized
-
-                int best = max({
-                    dp[i-1][j][k-1],
-                    dp[i][j-1][k],
-                    dp[i-1][j][k],
-                    dp[i][j-1][k-1]
-                });
-
-                if (best < 0) continue;
-
-                if (j == k) 
-                    dp[i][j][k] = best + grid[i-1][j-1];
-                else 
-                    dp[i][j][k] = best + grid[i-1][j-1] + grid[i+j-k-1][k-1];
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            for(int k=1;k<=m;k++){
+                if(i==1 && j==1 && k==1) continue;
+                if(i+j-k<1 || i+j-k>n)  continue;
+                if(grid[i-1][j-1] == -1 || grid[i+j-k-1][k-1]==-1) continue;
+                dp[i][j][k] = max({dp[i-1][j][k],dp[i][j-1][k],dp[i-1][j][k-1],dp[i][j-1][k-1]});
+                if(j==k) dp[i][j][k]+=grid[i-1][j-1];
+                else dp[i][j][k]+=(grid[i-1][j-1]+grid[i+j-k-1][k-1]);
             }
         }
     }
-
-    return max(0, dp[n][m][m]);
-}
-
-
-};
+    return max(0,dp[n][m][m]);
+} };
