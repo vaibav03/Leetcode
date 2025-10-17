e2abdafc86e439a367d8e2be2a0d09873c5ca494
vaibav03 <107197417+vaibav03@users.noreply.github.com>
2025-10-17 02:10:23 -0700

    Time: 8 ms (46.2%), Space: 153.6 MB (93.82%) - LeetHub

---
+++ 1731-even-odd-tree/1731-even-odd-tree.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isEvenOddTree(TreeNode* root) {
+        if (!root) return true;
+        queue<TreeNode*> q;
+        q.push(root);
+        int level = 0;
+
+        while (!q.empty()) {
+            int size = q.size();
+            int prev = (level % 2 == 0) ? INT_MIN : INT_MAX;
+
+            for (int i = 0; i < size; ++i) {
+                TreeNode* node = q.front(); 
+                q.pop();
+
+                if (level % 2 == 0) { 
+                    if (node->val % 2 == 0 || node->val <= prev)
+                        return false;
+                } else { 
+                    if (node->val % 2 == 1 || node->val >= prev)
+                        return false;
+                }
+
+                prev = node->val;
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            level++;
+        }
+
+        return true;
+    }
+};
