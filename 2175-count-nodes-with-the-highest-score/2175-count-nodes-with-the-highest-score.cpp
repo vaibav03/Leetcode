d14d78cc93aad9f164060cf6d3d1b136b1839569
vaibav03 <107197417+vaibav03@users.noreply.github.com>
2025-10-17 20:59:21 -0700

    Time: 127 ms (81.13%), Space: 114.5 MB (95%) - LeetHub

--- 2175-count-nodes-with-the-highest-score/2175-count-nodes-with-the-highest-score.cpp
+++ 2175-count-nodes-with-the-highest-score/2175-count-nodes-with-the-highest-score.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
-    long long ans = 0;
-    int cnt = 0;
+    long long ans{};
+    int cnt{};
+    int dfs(vector<vector<int>> &adj, int v){
+        
 
-    int dfs(vector<vector<int>> &adj, int v, int n) {
-        long long score = 1;  
-        int size = 1;         
+        int right=0,left=0;
+        if(adj[v].size()>=1)
+        right = dfs(adj,adj[v][0]);
+        if(adj[v].size()>=2)
+        left = dfs(adj,adj[v][1]);
 
-        for (int child : adj[v]) {
-            int sub = dfs(adj, child, n);
-            score *= sub;   
-            size += sub;
-        }
+        int curr_r = (right == 0) ? 1 : right;
+        int curr_l = (left == 0) ? 1 : left ;
+        int x = (adj.size() - left - right - 1 == 0 ) ? 1 : adj.size() - left - right - 1;
+        long long curr = 1ll*curr_r*curr_l*(x); 
 
-        int remaining = n - size; 
-        if (remaining > 0)
-            score *= remaining;    
-
-        if (score > ans) {
-            ans = score;
-            cnt = 1;
-        } else if (score == ans) {
-            cnt++;
-        }
-
-        return size;
+            if(curr > ans){
+                cnt = 1;
+                ans = curr;
+            }else if(curr == ans){
+                cnt++;
+            }
+        return left+right+1;
     }
-
     int countHighestScoreNodes(vector<int>& parents) {
         int n = parents.size();
         vector<vector<int>> adj(n);
+        for(int i=0;i<n;i++) {
+            if(parents[i]!=-1)
+            adj[parents[i]].push_back(i);
+            }
 
-        for (int i = 0; i < n; i++) {
-            if (parents[i] != -1)
-                adj[parents[i]].push_back(i);
-        }
-
-        dfs(adj, 0, n);
+        dfs(adj,0);
         return cnt;
     }
 };
