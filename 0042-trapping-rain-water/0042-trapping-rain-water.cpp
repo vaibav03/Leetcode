c100b31644dbe6bb4dff766663f69855c0fb8df7
vaibav03 <107197417+vaibav03@users.noreply.github.com>
2025-10-17 19:30:31 -0700

    Time: 352 ms (5.41%), Space: 27.3 MB (19.66%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        stack <int> st;
-        st.push(height[0]);
-        int i = 1,ans{},curr{}, n = height.size();
-
-        int prefix{},suffix{};
-        vector<int> pref(n,0),suff(n,0);
-        pref[0]=height[0];
-        for(int i=1;i<height.size();i++){
-           pref[i] = max(pref[i-1],height[i]);
+    int trap(vector<int>& heights) {
+        int ans{},n = heights.size();
+        vector<int> pre(n),suf(n);
+        pre[0] = heights[0];
+        suf[n-1] = heights[n-1];
+        for(int i=1;i<n;i++){
+            pre[i] = max(pre[i-1],heights[i]);
         }
-        suff[n-1] = height[n-1];
-        for(int i=height.size()-2;i>=0;i--){
-            suff[i] = max(suff[i+1],height[i]);
+        for(int i=n-2;i>=0;i--){
+            suf[i] = max(suf[i+1],heights[i]);
         }
 
-        while(i<height.size()){
-            ans+=(min(suff[i],pref[i])-height[i]);
-            i++;
+        for(int i=0;i<n;i++) cout<<pre[i]<<' ';
+
+        cout<<endl;
+        for(int i=0;i<n;i++) cout<<suf[i]<<' ';
+
+        for(int i=1;i<n-1;i++){
+            ans+= (min(pre[i],suf[i]) - heights[i]);
         }
+
         return ans;
     }
 };
