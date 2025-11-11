cf3900e3623a7ee66b1cb469939456d34d98b7d5
vaibav03 <107197417+vaibav03@users.noreply.github.com>
2025-11-11 09:50:51 -0700

    Time: 24 ms (78.39%), Space: 17.3 MB (53.77%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,47 +1,25 @@
 class Solution {
 public:
-void bfs(vector<vector<char>> &g, vector<vector<int>> &vis, int i, int j) {
-    queue<pair<int, int>> q;
-    q.push({i, j});
-    vis[i][j] = 1;
+    void bfs(vector<vector<char>>& grid,vector<vector<int>> & vis,int i, int j){
+        if(i>=grid.size() || j>=grid[0].size() || i<0 || j<0) return ;
 
-    
-    vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-    while (!q.empty()) {
-        auto node = q.front();
-        q.pop();
-
-        for (auto &dir : directions) {
-            int f = node.first + dir.first;
-            int s = node.second + dir.second;
-
-            
-            if (f >= 0 && f < g.size() && s >= 0 && s < g[0].size() && g[f][s] == '1' && !vis[f][s]) {
-                q.push({f, s});
-                vis[f][s] = 1;
-            }
+        if(grid[i][j] == '1' && !vis[i][j]){
+            vis[i][j] = 1;
+            bfs(grid,vis,i+1,j);
+            bfs(grid,vis,i-1,j);
+            bfs(grid,vis,i,j+1);
+            bfs(grid,vis,i,j-1);
         }
     }
-}
-
-int numIslands(vector<vector<char>>& grid) {
-    if (grid.empty()) return 0;
-
-    int rows = grid.size();
-    int cols = grid[0].size();
-    vector<vector<int>> vis(rows, vector<int>(cols, 0));
-    int ans = 0;
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            if (!vis[i][j] && grid[i][j] == '1') {
-                bfs(grid, vis, i, j);
-                ans++;
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        int islands{};
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] && grid[i][j] == '1'){ bfs(grid,vis,i,j); islands++;}
             }
         }
+    return islands;
     }
-    return ans;
-}
-
 };
