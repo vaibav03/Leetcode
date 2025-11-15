070480be8842fdba8b12568552bc02b14918b07a
vaibav03 <107197417+vaibav03@users.noreply.github.com>
2025-11-14 19:42:15 -0700

    Time: 1890 ms (10.6%), Space: 640.2 MB (19.56%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
-    bool compare(string s1,string s2){
-        int i{},j{},inc{};
-        if(s1.size() - s2.size() != 1) return false;
-        while(i<s1.size() && j<s2.size()){
-            if(s1[i]==s2[j]){
-            i++;
-            j++;
-            }else{
-                inc++;
+    bool comp(string a,  string b){
+        if(b.size() - a.size() != 1 ) return false;
+        int i{},j{},differ{};
+        while( i < a.size() && j < b.size() ){
+            if(a[i]!=b[j]){
+                differ++;
+                j++;
+            }else {
                 i++;
+                j++;
             }
         }
-        return (inc<=1) ? true : false;
-    }
-    static bool cmp(string s1,string s2){
-        return s1.size()<s2.size() ; 
+        return (j == b.size() - 1 || differ ==1 ); 
     }
     int longestStrChain(vector<string>& words) {
-        sort(words.begin(),words.end(),cmp);
+        sort(words.begin(),words.end(),[](string &a,string &b){
+            return a.size() < b.size();
+        });
         int n = words.size();
         vector<int> dp(n,1);
         for(int i=0;i<n;i++){
-            cout<<words[i]<<" ";
             for(int j=0;j<i;j++){
-                if(compare(words[i],words[j])==1 && dp[j]+1>dp[i]){
-                    dp[i] = dp[j] + 1;
-                }
+                if(comp(words[j],words[i])) dp[i] = max(dp[j]+1,dp[i]);
             }
         }
-        cout<<endl;
-        for(int i=0;i<n;i++) cout<<dp[i]<<" ";
-        cout<<endl;
         return *max_element(dp.begin(),dp.end());
     }
 };
