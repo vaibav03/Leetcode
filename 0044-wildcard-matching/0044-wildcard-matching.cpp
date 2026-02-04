5132dede175d4fc1f042a0a077ebc7cf1999854b
vaibav03 <107197417+vaibav03@users.noreply.github.com>
2026-02-03 21:19:19 -0700

    Time: 134 ms (10.91%), Space: 54.8 MB (12.41%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-
-    bool isMatch(string s1, string s2) {
-        int n = s1.size(), m = s2.size();
-        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
-
-        dp[0][0] = 1;
-        for(int i=0;i<s2.size();i++){
-          if(s2[i]=='*')
-          dp[0][i+1]=true;
-          else
-          break;
-        } 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-             if(s1[i-1]==s2[j-1] || s1[i-1]=='?' || s2[j-1]=='?')
-                dp[i][j]=dp[i-1][j-1];
-
-              else if(s1[i-1]=='*' || s2[j-1]=='*')
-              {
-               dp[i][j] = dp[i-1][j]|dp[i][j-1];
-               }
+    bool ans(string &s, string &p ,int i , int j,vector<vector<int>> &dp){
+        if(i == s.size() && j==p.size() ) return true;
+        if(j == p.size()) return false;
+        if(i == s.size()){
+            while(j<p.size() && p[j]=='*') j++;
+            return (j==p.size());
         }
-       
-    } 
-    return dp[n][m];
+        bool res = false;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(p[j] == '*'){ res|=ans(s,p,i+1,j,dp);  res|=ans(s,p,i,j+1,dp); }
+        else if(p[j] == '?') res|=ans(s,p,i+1,j+1,dp);
+        else if(s[i] == p[j]) res|=ans(s,p,i+1,j+1,dp);
+
+        return dp[i][j] = res;
+    }
+    bool isMatch(string s, string p) {
+        int n = s.size(), m = p.size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return ans(s,p,0,0,dp);
     }
 };
