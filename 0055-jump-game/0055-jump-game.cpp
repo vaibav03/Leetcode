d5bdbd9ef3b632a5a31519f0c1e5826b938d8911
vaibav03 <107197417+vaibav03@users.noreply.github.com>
2026-03-31 00:10:24 -0700

    Time: 0 ms (100%), Space: 52.2 MB (81.99%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
-
     bool canJump(vector<int>& nums) {
-        if(nums.size() == 1) return true;
-        vector<int> vis(nums.size(),0);
-        int least = INT_MAX;
-        for(int i = nums.size()-2;i>=0;i--){
-            if(nums[i]+i >= nums.size()-1 || nums[i]+i>=least){
-                 vis[i] = true;
-                 least = min(least,i);
-                 }
-
-        }
+        int maxiReach = 0, lim = 0;
         for(int i=0;i<nums.size();i++){
-            cout<<vis[i]<<" ";
+            if(maxiReach < i) return false;
+            maxiReach = max(maxiReach, nums[i] + i);
         }
-        cout<<endl;
-        return vis[0];
+        return true;
     }
 };
