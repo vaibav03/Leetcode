83daef66f2957f64e4bbab6ad30586db2c9ec752
vaibav03 <107197417+vaibav03@users.noreply.github.com>
2026-03-31 14:04:33 -0700

    Time: 0 ms (100%), Space: 18.2 MB (5.9%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,19 +1,50 @@
 class Solution {
 public:
-    int maxProduct(vector<int>& nums) {
-        int n = nums.size(),ans=nums[0],st=1,end=1;
-        for(int i=0;i<n;i++){
-            if(st==0){
-                st = 1;
+int findAns(vector<int> &nums){
+    if(nums.empty()) return INT_MIN;
 
-            }
-            if(end==0){
-                end = 1;
-            }
-         st*=nums[i];
-         end*=nums[n-i-1];
-         ans = max(st,max(end,ans));
+    if(nums.size() == 1) return nums[0];
+
+        int neg{},ans = 1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] < 0) neg++;
         }
+        if(neg%2 == 0){
+            for(auto x : nums) {
+                ans*=x;
+            }
+            return ans;
+        }else{
+            int maxi1 = 1,maxi2 = 1;
+            int dupe = 0;
+            for(int i=0;i<nums.size();i++){
+                if(nums[i]<0) dupe++;
+                if(dupe == neg) break;
+                maxi1*=nums[i];
+            }
+            dupe = 0;
+            for(int i=nums.size()-1;i>=0;i--){
+                if(nums[i]<0) dupe++;
+                if(dupe == neg ) break;
+                maxi2*=nums[i];
+            }
+            cout<<maxi1<<" "<<maxi2<<endl;
+            return max(maxi1,maxi2);
+} }
+
+    int maxProduct(vector<int>& nums) {
+        vector<int> curr;
+        int ans = INT_MIN;
+        for(auto x : nums){
+            if(x == 0){
+                ans = max({ans , findAns(curr),0});
+                curr.clear();
+            }else{
+            curr.push_back(x);
+            }
+        }
+        ans = max(ans ,findAns(curr));
         return ans;
     }
+    
 };
