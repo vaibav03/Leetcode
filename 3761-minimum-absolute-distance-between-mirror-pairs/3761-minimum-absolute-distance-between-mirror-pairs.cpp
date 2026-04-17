a0eaa3f96fe853c52287072884edf6455d205efe
vaibav03 <107197417+vaibav03@users.noreply.github.com>
2026-04-16 17:25:52 -0700

    Time: 342 ms (14.46%), Space: 166.7 MB (11.27%) - LeetHub

--- 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     int reverse(int n){
-        int rev{},tot_pow{},powe{};
-        int dupe = n;
-        while(dupe){
-            dupe/=10;
-            tot_pow++;
-        }
-        
+        int ans{},powe  = 0, dupe = n;
+        while(n/(long long )pow(10,powe) !=0){  powe++;}
 
+        powe --;
+        //cout<< powe<<endl;
         while(n){
-            rev+=(pow(10,tot_pow - powe-1)*(n%10));
-            powe++;
+            ans += ((n%10) * pow(10,powe));
+            powe--;
             n/=10;
         }
-        return rev;
+    return ans;
     }
     int minMirrorPairDistance(vector<int>& nums) {
-        unordered_map<int,int> hash;
-        int ans = INT_MAX;
 
+        unordered_map<int,vector<int>> mpp;
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]].push_back(i);
+        }
+        int ans = INT_MAX;
         for(int i=0;i<nums.size();i++){
             int rev = reverse(nums[i]);
+            if(!mpp.count(rev) ) continue;
 
-            if(hash.count(nums[i]))
-            ans = min(ans, i - hash[nums[i]]);
-
-            hash[rev] = i;
+            int ind = lower_bound(mpp[rev].begin(),mpp[rev].end(),i) - mpp[rev].begin(),k = mpp[rev].size();
+        if(ind != k && mpp[rev][ind] > i) ans = min(ans, mpp[rev][ind]- i);
+        else if(ind < k-1 && mpp[rev][ind] == i) ans = min(ans, mpp[rev][ind+1]- i);
         }
         return (ans == INT_MAX) ? -1 : ans;
     }
