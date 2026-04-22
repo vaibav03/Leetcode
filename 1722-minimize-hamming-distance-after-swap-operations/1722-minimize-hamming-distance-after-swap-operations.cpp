16a4b826cb7b539e5e44a584f1a0d87240952c20
vaibav03 <107197417+vaibav03@users.noreply.github.com>
2026-04-21 17:08:47 -0700

    Time: 1336 ms (5.27%), Space: 186.6 MB (13.69%) - LeetHub

--- 1722-minimize-hamming-distance-after-swap-operations/1722-minimize-hamming-distance-after-swap-operations.cpp
+++ 1722-minimize-hamming-distance-after-swap-operations/1722-minimize-hamming-distance-after-swap-operations.cpp
@@ -1,52 +1,62 @@
 class Solution {
-private:
-    vector<int> fa;
-    vector<int> rank;
-    // path compression
-    int find(int x) {
-        if (fa[x] != x) {
-            fa[x] = find(fa[x]);
-        }
-        return fa[x];
-    }
-
-    void Union(int x, int y) {
-        x = find(x);
-        y = find(y);
-        if (x == y) return;
-        // merge by rank
-        if (rank[x] < rank[y]) {
-            swap(x, y);
-        }
-        fa[y] = x;
-        if (rank[x] == rank[y]) {
-            rank[x]++;
-        }
-    }
-
 public:
-    int minimumHammingDistance(vector<int>& source, vector<int>& target,
-                               vector<vector<int>>& allowedSwaps) {
+
+    class DSU{
+        public:
+        vector<int> parent;
+        vector<int> rank;
+        DSU(int n){
+            parent.resize(n, -1);
+            rank.resize(n,0);
+            for(int i=0;i<parent.size();i++){
+                parent[i] = i;
+            }
+        }
+        int findParent(int u){
+            if(parent[u] == u) return u;
+            
+            return parent[u] = findParent(parent[u]);
+        }
+        void Union(int u ,int v){
+            int pu = findParent(u),pv = findParent(v);
+            if(pu == pv ) return ;
+
+            if(rank[pu] > rank[pv]){
+                parent[pv] = pu;
+                rank[pu]++;
+            }else {
+                parent[pu] = pv;
+                rank[pv]++;
+            }
+        } 
+    };
+    int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
         int n = source.size();
-        fa.resize(n);
-        rank.resize(n, 0);
-        for (int i = 0; i < n; i++) {
-            fa[i] = i;
+
+        DSU dsu(n);
+        for(auto x : allowedSwaps){
+            dsu.Union(x[0],x[1]);
         }
-        for (auto& pair : allowedSwaps) {
-            Union(pair[0], pair[1]);
+        unordered_map<int, unordered_map<int,int>> mpp;
+        for(int i=0;i<source.size();i++){
+            int par = dsu.findParent(i);
+
+            mpp[par][source[i]]++;
         }
-        unordered_map<int, unordered_map<int, int>> sets;
-        for (int i = 0; i < n; i++) {
-            int f = find(i);
-            sets[f][source[i]]++;
+
+        for(auto x : mpp){
+            for(auto y : mpp[x.first]){
+                cout<<x.first<<" "<<y.first<<" "<<y.second<<endl;
+            }
         }
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            int f = find(i);
-            if (sets[f][target[i]] > 0) {
-                sets[f][target[i]]--;
-            } else {
+        int ans{};
+        cout<<" befifebj"<<endl;
+        for(int i=0;i<source.size();i++){
+            int par = dsu.findParent(i);
+            //cout<<par<<" "<<target[i]<<" "<<mpp[par][target<<endl;
+            if(mpp[par].count(target[i]) > 0 && mpp[par][target[i]]>0 ){
+                mpp[par][target[i]]--;
+            }else{
                 ans++;
             }
         }
