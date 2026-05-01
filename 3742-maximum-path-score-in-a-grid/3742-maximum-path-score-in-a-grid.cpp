8eecdcaaf9d4e32fd67ea68b0c0a0f205263d9b4
vaibav03 <107197417+vaibav03@users.noreply.github.com>
2026-04-30 17:06:57 -0700

    Time: 1709 ms (5.07%), Space: 729.4 MB (5.06%) - LeetHub

--- 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
+++ 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
     int maxPathScore(vector<vector<int>>& grid, int k) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<vector<int>>> dp(
-            m, vector<vector<int>>(n, vector<int>(k + 1, INT_MIN)));
-        dp[0][0][0] = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int c = 0; c <= k; c++) {
-                    if (dp[i][j][c] == INT_MIN) continue;
-                    if (i + 1 < m) {
-                        int val = grid[i + 1][j];
-                        int cost = (val == 0 ? 0 : 1);
-                        if (c + cost <= k) {
-                            dp[i + 1][j][c + cost] =
-                                max(dp[i + 1][j][c + cost], dp[i][j][c] + val);
-                        }
-                    }
-                    if (j + 1 < n) {
-                        int val = grid[i][j + 1];
-                        int cost = (val == 0 ? 0 : 1);
-                        if (c + cost <= k) {
-                            dp[i][j + 1][c + cost] =
-                                max(dp[i][j + 1][c + cost], dp[i][j][c] + val);
-                        }
-                    }
-                }
+        int n = grid.size(), m = grid[0].size();
+       vector<vector<vector<long long>>>dp(n,vector<vector<long long>>(m,vector<long long>(k+1,INT_MIN)));
+       for(int i=0;i<=k;i++) dp[0][0][i] = 0;
+       for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            for(int x = 0;x<=k;x++){
+                long long curr=INT_MIN,cost = (grid[i][j] == 0) ? 0 : 1;
+                if(i == 0 && j ==0) continue;
+                if(i>0 && x-cost>=0) curr = max(curr, dp[i-1][j][x-cost]+grid[i][j]);
+
+                if(j>0 && x-cost>=0) curr = max(curr, dp[i][j-1][x-cost]+grid[i][j]);
+
+                dp[i][j][x] = max(curr,dp[i][j][x]);
             }
         }
-        int ans = INT_MIN;
+       }
+    long long ans = INT_MIN;
         for (int c = 0; c <= k; c++) {
-            ans = max(ans, dp[m - 1][n - 1][c]);
+            ans = max(ans, dp[n - 1][m - 1][c]);
         }
         return ans < 0 ? -1 : ans;
     }
